725c3a62407c21899a8a11523bf5a8bde988eabc
goozgk <2071735836@qq.com>
2017-09-20 13:30:28 +0800

    add

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by kangzefu on 2017/9/20.
+//  Copyright © 2017年 yuanfang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
